cda207ddf3dec692828aa03c56cfac41c1d6f6ac
Alice322 <rabover@yandex.ru>
2021-05-19 17:06:12 +0400

    feat: implement add and delete todo

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector('.todo-list')
 
 //Event-listeners
 todoButton.addEventListener('click', addTodo)
+todoList.addEventListener('click', deleteCheck)
 
 //functions
 function addTodo(event) {
@@ -20,6 +21,36 @@ function addTodo(event) {
     newTodo.classList.add('todo-item')
     todoDiv.appendChild(newTodo)
     //check mark button
-
+    const completedButton = document.createElement('button')
+    completedButton.innerHTML = '<i class="fas fa-check"></i>'
+    completedButton.classList.add('complete-btn')
+    todoDiv.appendChild(completedButton)
     //trash button
+    const trashButton = document.createElement('button')
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>'
+    trashButton.classList.add('trash-btn')
+    todoDiv.appendChild(trashButton)
+    //append to list
+    todoList.appendChild(todoDiv)
+    //clear todo input value
+    todoInput.value = ''
+}
+
+//delete element
+function deleteCheck(e) {
+    const item = e.target
+    if (item.classList[0] === 'trash-btn') {
+        // item.remove()
+        const todo = item.parentElement
+        //animation
+        todo.classList.add('fall')
+        todo.addEventListener('transitionend', function() {
+            todo.remove()
+        }) 
+    }
+
+    if(item.classList[0] === 'complete-btn') {
+        const todo = item.parentElement
+        todo.classList.toggle('completed')
+    }
 }
